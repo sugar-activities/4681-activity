f3028290439365bd115d48b096d1c02c0d64678c
Ignacio Rodriguez <ignacio@sugarlabs.org>
2017-04-21 01:04:44 -0400

    Initial commit

---
+++ lessons.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+from gettext import gettext as _
+import glib
+
+LESSONS = (
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 5')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Rule #4: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('We have an opponent in the game of life.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points: ')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • We each have two inner voices guiding us in life: Our True Voice encourages us to make an effort, share, care and think of ourselves and others. Our Opponent Voice challenges us to take the easy way out and thinking only of ourselves.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • We are the only ones who can hear what our voices are telling us.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • We can distinguish between the two voices by asking ourselves “Am I thinking of myself and others?” or “Am I thinking of myself only?”')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Our Opponent Voice gives us the opportunity to overcome our selfishness and win the Game of Life.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Words to Know:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('True Voice: The voice within that encourages me to make an effort, share, care and think of myself and others; connects me to the Light.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Opponent Voice: The voice within that challenges me to take the easy way out and think only of myself.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Think of a time you chose to listen to your True Voice? What happened? How did you feel?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Think of a time you chose to listen to your Opponent’s voice? What happened? How did you feel?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 6 (Part 1)')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Rule #5: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('We have an opponent in the game of life.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points: ')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • We are the only ones who can choose which voice to listen to.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Listening to our True Voice leads to long-lasting fulfillment.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Listening to our Opponent Voice leads to temporary fulfillment.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Words to Know:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Long-Lasting Fulfillment: The result of listening to our True Voice; requires effort but feels good over time ')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Temporary Fulfillment: The result of listening to our Opponent Voice; might feel good for a short amount of time but feels bad later on.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • What is the difference between long-lasting fulfillment and temporary fulfillment?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • What choices did you make recently that gave you only temporary fulfillment?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • What choices did you make recently that gave you long-lasting fulfillment?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 6 (Part 2)')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Rule #5: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('We have an opponent in the game of life.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points: ')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • We are the only ones who can choose which voice to listen to.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Listening to our True Voice leads to long-lasting fulfillment.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Listening to our Opponent Voice leads to temporary fulfillment.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Words to Know:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Long-Lasting Fulfillment: The result of listening to our True Voice; requires effort but feels good over time ')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Temporary Fulfillment: The result of listening to our Opponent Voice; might feel good for a short amount of time but feels bad later on.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • What is the difference between long-lasting fulfillment and temporary fulfillment?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • What choices did you make recently that gave you only temporary fulfillment?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • What choices did you make recently that gave you long-lasting fulfillment?')),
+)
---
+++ videos.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+VIDEOS = (
+    ("5.ogv",
+     _("My Opponent"),
+     _("Rule #4: We have an opponent in the game of life\n\
+We each have two inner voices guiding us in life: Our True Voice encourages us to make an effort, share, care and think of ourselves and others. Our Opponent Voice challenges us to take the easy way out and thinking only of ourselves."),
+         ),
+    ("6.1.ogv",
+     _("My Choices (Part 1)"),
+     _("Rule #5: We can choose to listen to our True Voice at any time.\n\
+We are the only ones who can choose which voice to listen to.\n\
+Listening to our True Voice leads to long-lasting fulfillment.\n\
+Listening to our Opponent Voice leads to temporary fulfillment."),
+    ),
+    ("6.4.ogv",
+     _("My Choices (Part 2)"),
+     _("Rule #5: We can choose to listen to our True Voice at any time.\n\
+We are the only ones who can choose which voice to listen to.\n\
+Listening to our True Voice leads to long-lasting fulfillment.\n\
+Listening to our Opponent Voice leads to temporary fulfillment."),
+    ),
+)
